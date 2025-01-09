c9154a9b028bd2d56ecb156378ce22532842a4eb
Vinicius Faria <viniciusfariaresende@gmail.com>
2025-01-08 21:25:07 -0300

    feat: added client sh file

---
+++ client/client.sh
@@ -0,0 +1,5 @@
+wget --server-response \
+    --output-document response.out \
+    --header='Content-Type: application/json' \
+    --post-data '{"songs": ["HUMBLE.", "DNA.", "goosebumps"]}' \
+    http://10.43.253.217:30502/api/recommend
