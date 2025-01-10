c5a15638c22c861d29db62e28552fd8c21dadd42
Vinicius Faria <viniciusfariaresende@gmail.com>
2025-01-10 11:34:11 -0300

    feat: changing songs on client script

--- client/client.sh
+++ client/client.sh
@@ -1,5 +1,5 @@
 wget --server-response \
     --output-document response.out \
     --header='Content-Type: application/json' \
-    --post-data '{"songs": ["HUMBLE.", "DNA.", "goosebumps"]}' \
+    --post-data '{"songs": ["This Is How We Roll", "Crash And Burn", "Ready Set Roll"]}' \
     http://10.43.253.217:30502/api/recommend
