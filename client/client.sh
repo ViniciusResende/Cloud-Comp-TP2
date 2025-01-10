64865e3c81585200ef2078ae458724edfe5d3716
Vinicius Faria <viniciusfariaresende@gmail.com>
2025-01-10 14:52:00 -0300

    feat: editing client script to give output in Json format

--- client/client.sh
+++ client/client.sh
@@ -1,5 +1,5 @@
 wget --server-response \
-    --output-document response.out \
+    --output-document response.json \
     --header='Content-Type: application/json' \
     --post-data '{"songs": ["This Is How We Roll", "Crash And Burn", "Ready Set Roll"]}' \
     http://10.43.253.217:30502/api/recommend
